ca6d8ec63c41a70d9c10eb823bbab1d10cfaac18
mesiobass <mes10bass@gmail.com>
2025-10-31 11:30:06 +0900

    deviseでユーザモデル実装

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,8 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  config.action_mailer.default_url_options = { host: ENV.fetch("APP_HOST", "example.com"), protocol: ENV.fetch("APP_PROTOCOL", "https") }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
