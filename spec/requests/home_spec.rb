25e30c36afcdd1b7489a3d81d59a9dc72718c071
mesiobass <mes10bass@gmail.com>
2025-10-31 11:34:57 +0900

    ログイン必須にする

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,8 +1,29 @@
 RSpec.describe "Home" do
   describe "GET /" do
-    it "returns http success" do
-      get "/"
-      expect(response).to have_http_status(:success)
+    context "ログインしていない場合" do
+      it "サインイン画面へリダイレクトする" do
+        get "/"
+
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    context "ログインしている場合" do
+      let(:user) do
+        User.create!(
+          email: "user@example.com",
+          password: "password",
+          password_confirmation: "password"
+        )
+      end
+
+      it "200を返す" do
+        sign_in user
+
+        get "/"
+
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 end
